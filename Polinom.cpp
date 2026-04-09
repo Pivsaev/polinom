45ac45968b3447f28df6e3985a075acf36cd063f
Pivsaev <144445002+Pivsaev@users.noreply.github.com>
2026-04-09 11:19:52 +0300

    Update Polinom.cpp

--- Polinom.cpp
+++ Polinom.cpp
@@ -16,34 +16,40 @@ Polinom& Polinom::operator=(const Polinom& p)
 
 void Polinom::AddMonom(Monom* monom)
 {
-	Link* l = list.FindBySvertka(monom->GetSvertka());
-	if (l != nullptr) {
-		l->GetMonomData()->AddKoeff(monom->GetKoeff());
-	}
-	else {
-		Link* linkk = new Link(monom);
-		list.Insert(linkk);
-	}
+    Link* l = list.FindBySvertka(monom->GetSvertka());
+    if (l != nullptr) {
+        l->GetMonomData()->AddKoeff(monom->GetKoeff());
+        delete monom;
+        if (l->GetMonomData()->GetKoeff() == 0) {
+            list.Remove(l);
+        }
+    }
+    else {
+        Link* linkk = new Link(monom);
+        list.Insert(linkk);
+    }
 }
 
 Polinom Polinom::operator+(Polinom& p) const
 {
     Polinom result;
+    Link* dummy1 = list.GetDummy();
     Link* curr = list.GetHead();
-    while (curr != nullptr) {
+    while (curr != dummy1) {
         Monom* m = curr->GetMonomData();
         Monom* newMonom = new Monom(m->GetKoeff(), m->GetSvertka());
         result.AddMonom(newMonom);
         curr = curr->GetNextLink();
     }
+    Link* dummy2 = p.list.GetDummy();
     curr = p.list.GetHead();
-    while (curr != nullptr) {
+    while (curr != dummy2) {
         Monom* m = curr->GetMonomData();
         Monom* newMonom = new Monom(m->GetKoeff(), m->GetSvertka());
         result.AddMonom(newMonom);
         curr = curr->GetNextLink();
     }
-    return result;;
+    return result;
 }
 
 Polinom::Polinom(const Polinom& other) : list(other.list) {}
@@ -51,8 +57,9 @@ Polinom::Polinom(const Polinom& other) : list(other.list) {}
 Polinom Polinom::AddBySvertka(const Polinom& other) const
 {
     Polinom result;
+    Link* dummy1 = list.GetDummy();
     Link* curr1 = list.GetHead();
-    while (curr1 != nullptr) {
+    while (curr1 != dummy1) {
         Monom* m1 = curr1->GetMonomData();
         int svertka1 = m1->GetSvertka();
         Link* found = other.list.FindBySvertka(svertka1);
@@ -64,37 +71,46 @@ Polinom Polinom::AddBySvertka(const Polinom& other) const
                 result.AddMonom(newMonom);
             }
         }
-
         curr1 = curr1->GetNextLink();
     }
-
     return result;
 }
 
 std::ostream& operator<<(std::ostream& out, const Polinom& p)
 {
-    if (p.list.IsEmpty()) {
-        out << "0";
-        return out;
-    }
+    Link* dummy = p.list.GetDummy();
     Link* curr = p.list.GetHead();
     bool first = true;
-    while (curr) {
+    bool printed = false;
+    while (curr != dummy) {
         Monom* m = curr->GetMonomData();
         int coeff = m->GetKoeff();
         if (coeff != 0) {
-            if (first) {
-                if (coeff < 0) out << "-";
-                out << *m;
-                first = false;
+            int absCoeff = (coeff < 0) ? -coeff : coeff;
+            int svertka = m->GetSvertka();
+            int degX = svertka / 100;
+            int degY = (svertka / 10) % 10;
+            int degZ = svertka % 10;
+            bool hasVars = (degX != 0 || degY != 0 || degZ != 0);
+            if (!first) {
+                out << (coeff > 0 ? " + " : " - ");
             }
-            else {
-                if (coeff > 0) out << " + ";
-                else out << " - ";
-                out << *m;
+            else if (coeff < 0) {
+                out << "-";
             }
+            if (absCoeff != 1 || !hasVars) {
+                out << absCoeff;
+            }
+            if (degX != 0) out << "x^" << degX;
+            if (degY != 0) out << "y^" << degY;
+            if (degZ != 0) out << "z^" << degZ;
+            first = false;
+            printed = true;
         }
         curr = curr->GetNextLink();
+    }
+    if (!printed) {
+        out << "0";
     }
     return out;
 }
